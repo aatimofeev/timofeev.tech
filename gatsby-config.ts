1c800ceeaf9d4eeb97c6b75a6c3699a94cbb7115
aatimofeev <eisenbart@ya.ru>
2024-04-21 17:54:13 +0300

    Update build scripts and site metadata
    The build and develop scripts in package.json have been updated to include the '--prefix-paths' flag. Additionally, a new deploy script was added. The site metadata in gatsby-config.ts has been updated with a new pathPrefix, siteTitle, siteTitleAlt, and siteUrl to reflect the new domain.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,13 +5,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 const config: GatsbyConfig = {
   siteMetadata: {
+      pathPrefix: "/timofeev.tech",
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitle: `Anton Timofeev's Blog`,
+    siteTitleAlt: `Anton Timofeev's Blog`,
     siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
+    siteUrl: `https://timofeev.tech`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
